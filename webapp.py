6f154036c6fa82e27c213c1b265f2ced2457773c
Stephen Okopi <okopistephen1@gmail.com>
2026-09-04 14:58:32 +0100

    webapp.py

--- webapp.py
+++ webapp.py
@@ -2,10 +2,38 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+    'id': 1,
+    'title': 'Frontend developer',
+    'location': 'Asa',
+    'salary': 'N100,000',
+    },
+
+    {
+    'id': 2,
+    'title': 'Backend developer',
+    'location': 'Asa 1',
+    'salary': 'N200,000',
+    },
+    {
+    'id': 3,
+    'title': 'Expert Hacker',
+    'location': 'Asa 3',
+    'salary': 'N300,000',
+    },
+    
+
+    
+]
+
+
+
 
 @app.route("/")
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', 
+    jobs=JOBS)
 
 
 if __name__ == "__main__":
