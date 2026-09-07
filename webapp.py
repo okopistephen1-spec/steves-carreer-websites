d550c7ce3013ec7b80c1b44304582ad187fb731c
Stephen Okopi <okopistephen1@gmail.com>
2026-09-07 11:09:49 +0100

    webapp.py

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -34,6 +34,10 @@ JOBS = [
 def hello():
     return render_template('home.html', 
     jobs=JOBS)
+
+@app. route("/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 
 if __name__ == "__main__":
